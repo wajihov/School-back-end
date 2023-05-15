2f2094fa0a847dfcc9a27227fd628c061081a68d
wajih boufaied <wboufaied@gopartner.fr>
2023-05-15 02:16:34 +0100

    Create Login JWT

---
+++ src/main/java/fr/anywr/school/domain/user/UserRepository.java
@@ -0,0 +1,13 @@
+package fr.anywr.school.domain.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	Optional<User> findByEmail(String email);
+	
+}
---
+++ src/test/java/fr/anywr/school/user/UserRepositoryTests.java
@@ -0,0 +1,34 @@
+package fr.anywr.school.user;
+
+import fr.anywr.school.domain.user.User;
+import fr.anywr.school.domain.user.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Test
+    public void testCreateUser() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String password = passwordEncoder.encode("user123");
+
+        User newUser = new User("wajihbfd@hotmail.fr", password);
+        User savedUser = repo.save(newUser);
+
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+}
